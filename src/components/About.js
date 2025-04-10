76c150b05b8f3b38450f91daa9dc881c355c0170
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-04-10 17:32:36 +0300

    Completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>
+      I am a web developer with a passion for creating dynamic and responsive web applications.
+      I have experience in various technologies and frameworks, and I am always eager to learn more.
+    </p>
+    <img src={image} alt="I made this" />
+
+  </div>
+);
 }
 
 export default About;
